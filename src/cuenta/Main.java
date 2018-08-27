53ec34a306ace126226ff827ffb933b01ab9e84f
Estudiante <Estudiante@10.203.1.111>
2018-08-27 08:53:25 -0500

    }

--- src/cuenta/Main.java
+++ src/cuenta/Main.java
@@ -5,16 +5,40 @@
  */
 package cuenta;
 
+
 public class Main {
     Cuenta.global = 50;
     
     public static void main(String[] args){
+        
+    public static void main();{
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Ingrese el numero de la cuenta");
+        int cuenta = lectura.nextInt();
+        
+        System.out.println("Ingrese la clalve");
+        int clave = lectura.nextInt();
+        
+        System.out.println("Ingrese dueno");
+        int dueno = lectura.nextInt();
+        
+        System.out.println("Saldo");
+        int saldo = lectura.nextInt();
+        
+        Cuenta cl = null;
+        cl.consignar(1000000);
+        
+}
+        
+        
         Cuenta cuenta = new Cuenta(1,123,"f",100);
         
         System.out.println("Numero" + cuenta.numero);
         System.out.println("Clave" + cuenta.clave);
         System.out.println("Dueno" + cuenta.dueno);
-        System.out.println("Saldo" + cuenta.saldo);gfff
+        System.out.println("Saldo" + cuenta.saldo);
+        
+        
     }
     
 }
